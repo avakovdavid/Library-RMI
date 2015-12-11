f756c40ae28619f1fdba26ecd892c414f40b0575
Martine Labardy <martine.labardy@gmail.com>
2015-12-11 15:54:32 +0100

    commit test

--- src/fr/upem/library/client/LibraryClient.java
+++ src/fr/upem/library/client/LibraryClient.java
@@ -5,7 +5,7 @@ import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.text.ParseException;
 
-
+// 
 public class LibraryClient {
 
 	public static void main(String[] args) {
